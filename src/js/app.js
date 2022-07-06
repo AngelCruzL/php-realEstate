fc25e5c59881c603af4e0a531e84c8852c0ff5c1
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-06 12:38:17 -0500

    feat: add dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
 const $mobileMenu = document.querySelector('.mobileMenu');
+const $darkModeBtn = document.querySelector('.darkMode-btn');
+
 $mobileMenu.addEventListener('click', toggleMenu);
+$darkModeBtn.addEventListener('click', toggleDarkMode);
 
 function toggleMenu() {
   $mobileMenu.classList.toggle('active');
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle('darkMode');
 }
