1aacb59c3fe6cb93399ce6b445eef4ff7005500d
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-07 00:23:11 -0500

    fix: change database tables structure

--- queries.sql
+++ queries.sql
@@ -2,22 +2,36 @@ CREATE DATABASE bienes_raices;
 
 USE bienes_raices;
 
-CREATE TABLE vendedores(
+CREATE TABLE sellers(
   id INT(11) PRIMARY KEY AUTO_INCREMENT,
-  nombre VARCHAR(50) NOT NULL,
-  apellido VARCHAR(50) NOT NULL,
-  telefono VARCHAR(10) NOT NULL
+  firstname VARCHAR(50) NOT NULL,
+  lastname VARCHAR(50) NOT NULL,
+  phone VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE propiedades(
+CREATE TABLE estates(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  titulo VARCHAR(60),
-  precio DECIMAL(10,2),
-  imagen VARCHAR(200),
-  descripcion LONGTEXT,
-  habitaciones INT(1),
-  wc INT(1),
-  estacionamiento INT(1),
-  creado DATE,
-  vendedor_id INT(11) REFERENCES vendedores(id)
+  title VARCHAR(60),
+  price DECIMAL(10,2),
+  image VARCHAR(200),
+  description LONGTEXT,
+  bedrooms INT(1),
+  bathrooms INT(1),
+  park INT(1),
+  created_at DATE,
+  seller_id INT(11) REFERENCES seller(id)
+);
+
+INSERT INTO sellers(
+  firstname,
+  lastname,
+  phone
+) VALUES(
+  'John',
+  'Doe',
+  '1234567890'
+),(
+  'Luis',
+  'Lara',
+  '1234567890'
 );
