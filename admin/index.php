12979db37ddc8d043f0c0491c77f4a64fe2ddc3b
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-07 01:40:04 -0500

    feat: add estates table layout

--- admin/index.php
+++ admin/index.php
@@ -15,6 +15,31 @@ includeTemplate('header');
   <?php endif; ?>
 
   <a href="/bienes-raices/admin/estate/create.php" class="btnGreen">Nueva Propiedad</a>
+
+  <table class="estates">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Título</th>
+        <th>Imagen</th>
+        <th>Precio</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Casa en la playa</td>
+        <td><img src="/bienes-raices/images/5c3f7b96de4e4da036b0468a22b564b6.jpg" class="tableImage" alt="imagen table"></td>
+        <td>$1234000</td>
+        <td>
+          <a class="btnRed-block" href="#">Eliminar</a>
+          <a class="btnYellow-block" href="#">Actualizar</a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </main>
 
 <?php includeTemplate('footer') ?>
