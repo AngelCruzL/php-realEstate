e7601d052cb1d44ad312e6432974e047d39fc1d2
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-07 10:24:01 -0500

    feat: add functionality to delete data

--- admin/index.php
+++ admin/index.php
@@ -7,6 +7,23 @@ $estates = mysqli_query($db, $getEstatesQuery);
 
 $status = $_GET['status'] ?? null;
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $id = $_POST['id'];
+  $id = filter_var($id, FILTER_VALIDATE_INT);
+
+  if ($id) {
+    $getEstateImageQuery = "SELECT image FROM estates WHERE id = ${id};";
+    $result = mysqli_query($db, $getEstateImageQuery);
+    $estateImage = mysqli_fetch_assoc($result);
+    unlink('../images/' . $estateImage['image']);
+
+    $deleteQuery = "DELETE FROM estates WHERE id = ${id};";
+    $deleteEstate = mysqli_query($db, $deleteQuery);
+
+    if ($deleteEstate) header('Location: /bienes-raices/admin?status=3');
+  }
+}
+
 require '../includes/functions.php';
 includeTemplate('header');
 ?>
@@ -17,40 +34,48 @@ includeTemplate('header');
   <?php if (intval($status) === 1) : ?>
     <div class="alert alertSuccess">
       <p>Anuncio creado correctamente</p>
-    <?php elseif (intval($status) === 2) : ?>
-      <div class="alert alertSuccess">
-        <p>Anuncio actualizado correctamente</p>
-      </div>
-    <?php endif; ?>
+    </div>
+  <?php elseif (intval($status) === 2) : ?>
+    <div class="alert alertSuccess">
+      <p>Anuncio actualizado correctamente</p>
+    </div>
+  <?php elseif (intval($status) === 3) : ?>
+    <div class="alert alertSuccess">
+      <p>Anuncio eliminado correctamente</p>
+    </div>
+  <?php endif; ?>
 
-    <a href="/bienes-raices/admin/estate/create.php" class="btnGreen">Nueva Propiedad</a>
+  <a href="/bienes-raices/admin/estate/create.php" class="btnGreen">Nueva Propiedad</a>
 
-    <table class="estates">
-      <thead>
+  <table class="estates">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Título</th>
+        <th>Imagen</th>
+        <th>Precio</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <?php while ($estate = mysqli_fetch_assoc($estates)) : ?>
         <tr>
-          <th>ID</th>
-          <th>Título</th>
-          <th>Imagen</th>
-          <th>Precio</th>
-          <th>Acciones</th>
+          <td><?php echo $estate['id']; ?></td>
+          <td><?php echo $estate['title']; ?></td>
+          <td><img src="/bienes-raices/images/<?php echo $estate['image']; ?>" class="tableImage" alt="imagen table"></td>
+          <td>$<?php echo $estate['price']; ?></td>
+          <td>
+            <form method="POST">
+              <input type="hidden" name="id" value="<?php echo $estate['id'] ?>">
+              <input type="submit" class="btnRed-block w100" value="Eliminar" />
+            </form>
+            <a class="btnYellow-block" href="/bienes-raices/admin/estate/update.php?id=<?php echo $estate['id']; ?>">Actualizar</a>
+          </td>
         </tr>
-      </thead>
-
-      <tbody>
-        <?php while ($estate = mysqli_fetch_assoc($estates)) : ?>
-          <tr>
-            <td><?php echo $estate['id']; ?></td>
-            <td><?php echo $estate['title']; ?></td>
-            <td><img src="/bienes-raices/images/<?php echo $estate['image']; ?>" class="tableImage" alt="imagen table"></td>
-            <td>$<?php echo $estate['price']; ?></td>
-            <td>
-              <a class="btnRed-block" href="#">Eliminar</a>
-              <a class="btnYellow-block" href="/bienes-raices/admin/estate/update.php?id=<?php echo $estate['id']; ?>">Actualizar</a>
-            </td>
-          </tr>
-        <?php endwhile; ?>
-      </tbody>
-    </table>
+      <?php endwhile; ?>
+    </tbody>
+  </table>
 </main>
 
 <?php
