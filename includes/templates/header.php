61718b9c657562d666d50f9ccfb3ccc00da5e8dc
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-06 21:46:25 -0500

    fix: anchortags redirections

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -6,31 +6,31 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Bienes Raíces</title>
-  <link rel="stylesheet" href="../build/css/app.css" />
-  <script src="../build/js/bundle.min.js" defer></script>
+  <link rel="stylesheet" href="/bienes-raices/build/css/app.css" />
+  <script src="/bienes-raices/build/js/bundle.min.js" defer></script>
 </head>
 
 <body>
   <header class="header <?php echo $isIndex ? 'mainHeader' : '' ?>">
     <div class="container headerContent">
       <div class="navbar <?php echo $isIndex ? 'home' : '' ?>">
-        <a href="index.php">
-          <img src="../build/img/logo.svg" alt="Bienes Raíces logo" />
+        <a href="/bienes-raices/src/index.php">
+          <img src="/bienes-raices/build/img/logo.svg" alt="Bienes Raíces logo" />
         </a>
 
         <div class="mobileMenu">
-          <img src="../build/img/barras.svg" alt="Icono de menu responsivo" />
+          <img src="/bienes-raices/build/img/barras.svg" alt="Icono de menu responsivo" />
         </div>
 
         <div class="navigationContainer">
-          <img src="../build/img/dark-mode.svg" alt="" class="darkMode-btn" />
+          <img src="/bienes-raices/build/img/dark-mode.svg" alt="" class="darkMode-btn" />
 
           <nav class="mainNavigation <?php echo $isIndex ? 'home' : '' ?>">
             <ul>
-              <li><a href="about.php">Nosotros</a></li>
-              <li><a href="announcements.php">Anuncios</a></li>
-              <li><a href="blog.php">Blog</a></li>
-              <li><a href="contact.php">Contacto</a></li>
+              <li><a href="/bienes-raices/src/about.php">Nosotros</a></li>
+              <li><a href="/bienes-raices/src/announcements.php">Anuncios</a></li>
+              <li><a href="/bienes-raices/src/blog.php">Blog</a></li>
+              <li><a href="/bienes-raices/src/contact.php">Contacto</a></li>
             </ul>
           </nav>
         </div>
