7eb897ff8d610ee8146bce8bda6d9d8e4fd3dc08
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-06 21:49:43 -0500

    feat: add admin home page

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+require '../includes/functions.php';
+includeTemplate('header');
+?>
+
+<main class="container section">
+  <h1>Administrador de bienes raíces</h1>
+
+  <a href="/bienes-raices/admin/estate/create.php" class="btnGreen">Nueva Propiedad</a>
+</main>
+
+<?php includeTemplate('footer') ?>
