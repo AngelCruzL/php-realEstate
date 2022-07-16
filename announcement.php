09fd298ebdf623aab3a7b2fda9fa406c93a5ed57
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-16 16:58:52 -0500

    refactor: announcement templates to Active Record

--- announcement.php
+++ announcement.php
@@ -5,48 +5,41 @@ $id = filter_var($id, FILTER_VALIDATE_INT);
 if (!$id) header("Location: /bienes-raices/src");
 
 require 'includes/app.php';
-$db = dbConnection();
 
-$getEstateQuery = "SELECT * FROM estates WHERE id=${id}";
-$result = mysqli_query($db, $getEstateQuery);
+use App\Estate;
 
-if (!$result->num_rows) header("Location: /bienes-raices/src");
-
-$estate = mysqli_fetch_assoc($result);
+$estate = Estate::find($id);
 
 includeTemplate('header');
 ?>
 
 <main class="section container centerContent">
-  <h1><?php echo $estate['title']; ?></h1>
+  <h1><?php echo $estate->title; ?></h1>
 
-  <img loading="lazy" src="/bienes-raices/images/<?php echo $estate['image']; ?>" alt="anuncio" />
+  <img loading="lazy" src="/bienes-raices/images/<?php echo $estate->image; ?>" alt="anuncio" />
 
   <div class="propertyResume">
-    <p class="price"><?php echo $estate['price']; ?></p>
+    <p class="price"><?php echo $estate->price; ?></p>
 
     <ul class="featureIcons">
       <li>
         <img loading="lazy" src="build/img/icono_wc.svg" alt="Icono WC" />
-        <p><?php echo $estate['bathrooms']; ?></p>
+        <p><?php echo $estate->bathrooms; ?></p>
       </li>
 
       <li>
         <img loading="lazy" src="build/img/icono_estacionamiento.svg" alt="Icono WC" />
-        <p><?php echo $estate['bedrooms']; ?></p>
+        <p><?php echo $estate->bedrooms; ?></p>
       </li>
 
       <li>
         <img loading="lazy" src="build/img/icono_dormitorio.svg" alt="Icono WC" />
-        <p><?php echo $estate['park']; ?></p>
+        <p><?php echo $estate->park; ?></p>
       </li>
     </ul>
 
-    <p><?php echo $estate['description']; ?></p>
+    <p><?php echo $estate->description; ?></p>
   </div>
 </main>
 
-<?php
-includeTemplate('footer');
-mysqli_close($db);
-?>
+<?php includeTemplate('footer'); ?>
--- includes/templates/announcement.php
+++ includes/templates/announcement.php
@@ -1,44 +1,43 @@
 <?php
-$db = dbConnection();
 
-$getEstatesQuery = "SELECT * FROM estates LIMIT ${limit}";
-$estates = mysqli_query($db, $getEstatesQuery);
+use App\Estate;
+
+$estates = Estate::all();
+// debug($estates);
 ?>
 
 <div class="adsContainer">
-  <?php while ($estate = mysqli_fetch_assoc($estates)) : ?>
+  <?php foreach ($estates as $estate) : ?>
     <div class="ad">
 
-      <img loading="lazy" src="/bienes-raices/images/<?php echo $estate['image']; ?>" alt="anuncio" />
+      <img loading="lazy" src="/bienes-raices/images/<?php echo $estate->image; ?>" alt="anuncio" />
 
       <div class="content">
-        <h3><?php echo $estate['title']; ?></h3>
-        <p><?php echo $estate['description']; ?></p>
-        <p class="price">$<?php echo number_format($estate['price']); ?></p>
+        <h3><?php echo $estate->title; ?></h3>
+        <p><?php echo $estate->description; ?></p>
+        <p class="price">$<?php echo number_format($estate->price); ?></p>
 
         <ul class="featureIcons">
           <li>
             <img loading="lazy" src="build/img/icono_wc.svg" alt="Icono WC" />
-            <p><?php echo $estate['bathrooms']; ?></p>
+            <p><?php echo $estate->bathrooms; ?></p>
           </li>
 
           <li>
             <img loading="lazy" src="build/img/icono_estacionamiento.svg" alt="Icono WC" />
-            <p><?php echo $estate['bedrooms']; ?></p>
+            <p><?php echo $estate->bedrooms; ?></p>
           </li>
 
           <li>
             <img loading="lazy" src="build/img/icono_dormitorio.svg" alt="Icono WC" />
-            <p><?php echo $estate['park']; ?></p>
+            <p><?php echo $estate->park; ?></p>
           </li>
         </ul>
 
-        <a class="btnYellow-block" href="announcement.php?id=<?php echo $estate['id']; ?>">
+        <a class="btnYellow-block" href="announcement.php?id=<?php echo $estate->id; ?>">
           Ver propiedad
         </a>
       </div>
     </div>
-  <?php endwhile; ?>
+  <?php endforeach; ?>
 </div>
-
-<?php mysqli_close($db); ?>
