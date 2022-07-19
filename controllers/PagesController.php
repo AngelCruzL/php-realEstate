6ce00b0d1e0af81f58a468df029ca0664bc69984
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-18 21:40:13 -0500

    chore: setting mailtrap with phpMailer

--- controllers/PagesController.php
+++ controllers/PagesController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use MVC\Router;
 use Model\Estate;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class PagesController
 {
@@ -55,7 +56,29 @@ class PagesController
   public static function contact(Router $router)
   {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-      debug($_POST);
+      $email = new PHPMailer();
+      $email->isSMTP();
+      $email->Host = 'smtp.mailtrap.io';
+      $email->SMTPAuth = true;
+      $email->Port = 2525;
+      $email->Username = '9c9c0dc21affa6';
+      $email->Password = '9d1afd8104cbca';
+
+      $email->setFrom('admin@bienesraices.com');
+      $email->addAddress('admin@bienesraices.com', 'bienesraices.com');
+      $email->Subject = 'Contacto desde bienesraices.com';
+      $email->isHTML(true);
+      $email->CharSet = 'UTF-8';
+
+      $email->Body = '<h1>Contacto desde bienesraices.com</h1>
+        <p>Nombre: ' . $_POST['name'] . '</p>
+        <p>Email: ' . $_POST['email'] . '</p>
+        <p>Mensaje: ' . $_POST['message'] . '</p>';
+      if ($email->send()) {
+        echo 'mensaje enviado';
+      } else {
+        echo 'error al enviar el mensaje';
+      }
     }
 
     $router->render('pages/contact', []);
